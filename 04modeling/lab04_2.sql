b5e5d944a6f6b8a8a89d14ffaee3b9a9fcc2be8e
David Michel <djm43@calvin.edu>
2017-02-24 12:22:12 -0500

    Got more done on lab04_2

--- 04modeling/lab04_2.sql
+++ 04modeling/lab04_2.sql
@@ -26,7 +26,29 @@ INSERT INTO PersonTeam VALUES ('Shamkant', 'executive');
 INSERT INTO PersonVisit VALUES ('Shamkant', '22-FEB-2015');
 INSERT INTO PersonVisit VALUES ('Shamkant', '1-MAR-2015');
 
+select * from PersonTeam;
+select * from PersonVisit;
+
 -- Query a combined "view" of the data of the form View(name, team, visit).
 SELECT pt.personName, pt.teamName, pv.personVisit
 FROM PersonTeam pt, PersonVisit pv
 WHERE pt.personName = pv.personName;
+
+-- Exercise 4.2
+	-- a. 
+	-- PersonTeam:
+	-- 	personName, teamName => personName, teamName
+	
+	-- PersonVisit:
+	-- 	personName, personVisit => personName, personVisit
+	
+	-- These are in BCNF because every partial key depends on a superkey
+	
+	-- b.
+	-- PersonTeamVisit:
+	-- 	personName, teamName, personVisit => personName, teamName, personVisit
+	-- This table is 
+	
+	-- c.
+		-- No it does not, I would prefer the combined schemata. With the first value, there is no unique or primary key to connect the two tables together, as PersonName and TeamName do not determine personVisit. The answer also might depend on whether or not a person
+		
