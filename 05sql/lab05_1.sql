a5c54827760c411623cbb3cbf922236c171d17ee
davejoshmike <davejoshmike@gmail.com>
2017-03-04 16:56:32 -0500

    Progress made in lab yesterday

---
+++ 05sql/lab05_1.sql
@@ -0,0 +1,9 @@
+﻿-- Exercise 5.1
+-- a.
+    select count(*) from person, household;
+    -- There are 128 records queried. 
+    -- The query did the cross product of the rows in person (16 rows) and the rows in household (8 rows).
+
+-- b.
+    select * from person ORDER BY To_char(BIRTHDATE, 'DDD') DESC;
+    -- DDD computes the day of the year given a date
---
+++ 05sql/lab05_2.sql
@@ -0,0 +1,12 @@
+﻿-- Exercise 5.2
+-- a.
+    SELECT * FROM PERSON WHERE BIRTHDATE IS NOT NULL AND ROWNUM <=1 ORDER BY To_char(BIRTHDATE, 'YYYY') DESC;
+    --incomplete
+
+-- b.
+    select  p1.FIRSTNAME, p1.id, p1.LASTNAME, p2.id, p2.LASTNAME from Person p1, Person p2 WHERE p1.FIRSTNAME = p2.FIRSTNAME AND p1.id <> p2.id; 
+    -- incomplete   
+
+--c.
+    select p.FIRSTNAME||' '||p.LASTNAME, pt.TEAMNAME from PERSON p, PERSONTEAM pt, TEAM t where p.id=pt.personid AND pt.TEAMNAME='Music' AND p.homegroupid<>(select p.homegroupid from homegroup h where p.homegroupid = h.id AND h.NAME='Byl');
+    -- incomplete
