a1ece15e143ec6a7ec114ca9e7ebb3408d7e73fd
davejoshmike <davejoshmike@gmail.com>
2017-03-07 21:02:36 -0500

    Finished the guide for unit 6

---
+++ 06sql/guide06.sql
@@ -0,0 +1,22 @@
+﻿-- Inner Join
+-- Select the movies that harrison ford was the star 
+    SELECT title, year, score
+    FROM ((Casting JOIN Performer ON performerId=id 
+            AND status='star' 
+            AND firstName='Harrison' 
+            AND lastName='Ford')
+        JOIN Movie m ON movieId=m.id)
+    ;
+
+-- Outer Join
+    select *
+        from (casting c LEFT OUTER JOIN movie m on c.movieid = m.id)
+        ;
+
+    select *
+        from (casting c RIGHT OUTER JOIN movie m on c.movieid = m.id)
+        ;
+
+-- Aggregate
+    select *, min(year)
+        from movies
