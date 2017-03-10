918217578af649a8e0ceab740c34d515678ef7df
davejoshmike <davejoshmike@gmail.com>
2017-03-10 13:12:34 -0500

    Finished lab06

---
+++ 06sql/lab06_1.sql
@@ -0,0 +1,14 @@
+﻿-- Exercise 6.1
+    --a. Get the names and mandate statements of all teams along with the ID of their “chair” member. If a chair member does not exist, include NULL for the ID.
+    select t.name, t.mandate, pt.personid "CHARID"
+    from (Team t LEFT OUTER JOIN PersonTeam pt 
+        ON pt.role='chair' 
+        AND t.name = pt.teamname);      
+
+    -- b.
+    select t.name, t.mandate, p.firstname || ' ' || p.lastname FULLNAME, pt.personid "CHARID"
+    from (Team t LEFT OUTER JOIN PersonTeam pt 
+        ON pt.role='chair' 
+        AND t.name = pt.teamname) 
+        LEFT OUTER JOIN Person p 
+        ON p.id=pt.personid;
---
+++ 06sql/lab06_2.sql
@@ -0,0 +1,23 @@
+﻿-- Exercise 6.2: Aggregates and Funcions
+-- a. Compute the average age of all the people in the database. Note you can use the following Oracle features in this query.
+    SELECT avg(months_between(trunc(sysdate), trunc(BIRTHDATE))/12) "Average Age" FROM Person;
+
+    -- This aggregate function operates on the entire table which is one giant group
+
+-- b. Get the household ID and count of members of all households in Grand Rapids having at least 2 members. Order the results by decreasing size
+    SELECT p.householdid, count(*), h.PHONENUMBER
+    FROM (Person p JOIN household h 
+        ON h.city = 'Grand Rapids') 
+    GROUP BY p.householdid, h.phonenumber
+    HAVING count(*) > 1 
+    ORDER BY count(*) DESC
+    ;
+    
+-- c. Modify the previous query to retrieve the phone number of the household as well.
+    SELECT p.householdid, count(*), h.PHONENUMBER
+    FROM (Person p JOIN household h 
+        ON h.city = 'Grand Rapids' AND p.householdid=h.id) 
+    GROUP BY p.householdid, h.phonenumber
+    HAVING count(*) > 1 
+    ORDER BY count(*) DESC
+    ;
