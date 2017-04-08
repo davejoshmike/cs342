83f519cfeeb31ff5b1a7cd2488049f662146be5b
davejoshmike <davejoshmike@gmail.com>
2017-04-08 02:23:02 -0400

    Cleaned up the homework file and added comments

--- 08programming/homework08_2.sql
+++ 08programming/homework08_2.sql
@@ -13,23 +13,35 @@
 -- If an actor works with another actor who worked with Kevin Bacon in a movie,
     -- the first actor's Bacon number is 2, and so forth.
 
--- Algo:
---base: set baconNum of the actual bacon to 0
+-- Old Algo:
+-- base: set baconNum of the actual bacon to 0
 --
---for each actorId in movieIds,
---check if baconNum is already set for that actor
+-- for each actorId in movieIds,
+-- check if baconNum is already set for that actor
 --         also check if the baconNum is greater than the current degree
 --             mark baconNum as current degree
 --     if baconNum is not set
 --         mark baconNum as current degree
---and
---get new movieIds that the actor has been in
---recursive call (for each actorid in movieIds...)
+-- and
+-- get new movieIds that the actor has been in
+-- then do a recursive call (for each actorid in movieIds...)
 
--- Note: before calling, run this on the system user:
-   -- ALTER SYSTEM SET open_cursors = 65535 SCOPE=BOTH;  
+-- basically, go to the farthest left node and go from there
+
+-- new algo:
+-- only select actors who are not in the bacon table yet
+    -- and are in a movie with kevin bacon (or in a movie with the lower degree
+    -- person)
+-- then for the whole depth, insert those nodes into the bacon table.
+    -- then we don't need to worry about updating or finding the lowest bacon number
+    -- and the amount of actors selected will diminish as more people are added
+    -- to the bacon table
+-- then for each actorid in the current depth
+    -- do the recursive call and once again updating the whole depth at once (for
+    -- the calling actor's movies)
 ---------------------------------------------------------------------------------
 
+-- make the Bacon table
 DROP TABLE Bacon;
 CREATE TABLE Bacon (
     actorId NUMBER(38),
@@ -38,13 +50,36 @@ CREATE TABLE Bacon (
     PRIMARY KEY (actorId)
 );
 
--- Note: default baconId is that of Kevin Bacon
-    -- default degreesFromBacon is recursive level 0
+-- compute all actors for current depth... wait till all are finished... then call recursive call of all actors from level 1
+CREATE OR REPLACE PROCEDURE computeForCurrentDepth (baconId NUMBER, degreesFromBacon NUMBER)
+IS
+    CURSOR newactors IS
+    (SELECT DISTINCT r.actorId
+    FROM Role r
+    WHERE r.movieId IN (SELECT r2.movieId 
+                        FROM Role r2 
+                        WHERE r2.actorId = computeForCurrentDepth.baconId)        
+    AND NOT EXISTS (SELECT *
+                    FROM Bacon b
+                    WHERE b.actorId = r.actorId)
+    AND r.actorId != baconId
+    );
+BEGIN
+    FOR newactor in newactors
+    LOOP
+        INSERT INTO Bacon VALUES (newactor.actorid, degreesFromBacon);
+    END LOOP;
+END;
+/
+
+-- calls ComputeForCurrentDepth and recursively calls itself to compute a bacon number
+-- for each actor in the Actor table.
 CREATE OR REPLACE PROCEDURE ComputeBaconNum (currentActorId NUMBER, baconId NUMBER, degreesFromBacon NUMBER)
 IS
+    --base:
     -- select the actorids from the movieids that kevin bacon has been in 
-    -- that arent in the bacon table
-    -- or are already kevin bacon
+        -- that arent in the bacon table
+        -- or are kevin bacon himself
     CURSOR newactors IS
     (SELECT DISTINCT r.actorId
     FROM Role r
@@ -57,152 +92,53 @@ IS
     AND r.actorId != ComputeBaconNum.currentActorId
     );
 
---    CURSOR existingactors IS
---    (SELECT DISTINCT r.actorId
---    FROM Role r
---    WHERE r.movieId IN (SELECT r2.movieId 
---                        FROM Role r2 
---                        WHERE r2.actorId=ComputeBaconNum.currentActorId)
---    AND EXISTS (SELECT *
---                FROM Bacon b
---                WHERE b.actorId = r.actorId
---                AND b.baconNum > ComputeBaconNum.degreesFromBacon)
---    AND r.actorId != ComputeBaconNum.currentActorId
---    );
     newDegreesFromBacon NUMBER;
+    l_actorid role.actorId%type;
 BEGIN
---    FOR existingactor in existingactors
---    LOOP
---        -- if the current Degree is closer than the actor's current baconNum, overwrite it with the better baconNum
---        -- The actor's existence in this table means we've already done our recursive call on their movies and don't need to again
---        UPDATE Bacon SET baconNum = degreesFromBacon WHERE actorId = existingactor.actorId;
---    END LOOP;
---    CLOSE existingactors;
     newDegreesFromBacon := degreesFromBacon + 1;
+    
+    -- stop at depth 5
     IF(newDegreesFromBacon > 5)
     THEN
         RETURN;
     END IF;
-    FOR newactor in newactors
+
+    -- compute entire depth at once
+    computeForCurrentDepth(baconId, newDegreesFromBacon);
+
+    --get the new actors for the next depth to compute
+    OPEN newactors;
+
     LOOP
-        -- if the actor doesn't have a baconNum, create one
-        INSERT INTO Bacon VALUES (newactor.actorId, newDegreesFromBacon);
-        ComputeBaconNum(newactor.actorId, baconId, newDegreesFromBacon);
+        FETCH newactors INTO l_actorId;
+        -- compute actors degrees from bacon
+        computeForCurrentDepth(l_actorId, newDegreesFromBacon);
+        EXIT WHEN l_actorId%notfound;
     END LOOP;
---    CLOSE newactors;
+    CLOSE newactors;
 END ComputeBaconNum;
 /
 
------------------------------------
-    --TEST--
-DECLARE
-    PROCEDURE test (currentActorId NUMBER, baconId NUMBER, degreesFromBacon NUMBER)
-    IS
-        CURSOR newactors IS
-        (SELECT DISTINCT r.actorId
-        FROM Role r
-        WHERE r.movieId IN (SELECT DISTINCT r2.movieId 
-                            FROM Role r2 
-                            WHERE r2.actorId = test.currentActorId)        
-        AND NOT EXISTS (SELECT *
-                        FROM Bacon b
-                        WHERE b.actorId = r.actorId)
-        AND r.actorId != test.currentActorId        
-        );
-    
-    --    CURSOR existingactors IS
-    --    (SELECT DISTINCT r.actorId
-    --    FROM Role r
-    --    WHERE r.movieId IN (SELECT r2.movieId 
-    --                        FROM Role r2 
-    --                        WHERE r2.actorId=ComputeBaconNum.currentActorId)
-    --    AND EXISTS (SELECT *
-    --                FROM Bacon b
-    --                WHERE b.actorId = r.actorId
-    --                AND b.baconNum > ComputeBaconNum.degreesFromBacon)
-    --    AND r.actorId != ComputeBaconNum.currentActorId
-    --    );
-        newDegreesFromBacon NUMBER;
-    BEGIN
-        newDegreesFromBacon := degreesFromBacon + 1;
-        IF(newDegreesFromBacon > 8)
-        THEN
-            RETURN;
-        END IF;
---        DBMS_OUTPUT.PUT_LINE('--------------------------------------------------------------------');
---        DBMS_OUTPUT.PUT_LINE('ActorId: ' || TO_CHAR(test.actorId));
---        DBMS_OUTPUT.PUT_LINE('Degrees From Bacon: ' || TO_CHAR(test.degreesFromBacon));
-
---        FOR existingactor in existingactors
---        LOOP
---            DBMS_OUTPUT.PUT_LINE('Existing Actor: ' || TO_CHAR(existingactor.actorId));
---        END LOOP;
-
-        FOR newactor in newactors
-        LOOP
-            
-            BEGIN
-                INSERT INTO Bacon VALUES (newactor.actorId, newDegreesFromBacon); 
-            EXCEPTION
-                WHEN OTHERS
-                THEN --DBMS_OUTPUT.PUT_LINE('PK violation on: ' || newactor.actorId);
-                NULL;
-            END;
-            test(newactor.actorId, baconId, newDegreesFromBacon);
-        END LOOP;
-    END;
-BEGIN
---    baconId := 225014;
-    DBMS_OUTPUT.PUT_LINE('---------------START----------------');
-    DBMS_OUTPUT.PUT_LINE('BaconId: ' || TO_CHAR(225014));
-    INSERT INTO Bacon VALUES (225014, 0); 
-    test(225014,225014,0);
-    DBMS_OUTPUT.PUT_LINE('---------------DONE----------------');
-END;
-/
-    delete from bacon;
-    Select count(*) from Bacon;
-    select baconnum, count(*) from bacon group by baconnum;
-
-    select movieid, count(*) from role group by movieid; --37 movies
-    select count(*) from actor where id in (select DISTINCT actorId from role); --1909 distinct actors
-------------------------------------
-
+-- does the base case and calls ComputeBaconNum
 CREATE OR REPLACE PROCEDURE PopulateBaconProc (baconId NUMBER DEFAULT 22591)
 IS      
---    Iterator NUMBER := 1;     
---    baconNum NUMBER;
 BEGIN
---    -- for loooooooops yaaaaaaaaaaaaaaaaay
---        FOR actor in (SELECT id FROM ACTOR)
---        LOOP
---            -- go through each actor and compute bacon number for each actor
---            baconNum := ComputeBaconNum(actor.Id);
---            --INSERT INTO Bacon VALUES (actor.Id, baconNum);
---        END LOOP;
-
     -- The one true Bacon. Base case.
     INSERT INTO Bacon VALUES (baconId, 0);
     ComputeBaconNum(baconId, baconId, 0);
 END PopulateBaconProc;
 /
 
+-- call our stored procedure
 DECLARE
     baconId NUMBER;
 BEGIN
---    SELECT id INTO baconId FROM Actor WHERE firstname='Kevin' AND lastname='Bacon';
-    baconId := 225014;
+    SELECT id INTO baconId FROM Actor WHERE firstname='Kevin' AND lastname='Bacon';
     PopulateBaconProc(baconId);
-
---    SELECT id INTO baconId FROM Actor WHERE firstname='Samuel L.' AND lastname='Jackson';
---    BaconProc(baconId);
---    Select * From Bacon;
 END;
 /
-DELETE FROM BACON;
-Select * From Bacon WHERE ROWNUM <= 10;
-select baconnum, count(*) From BACON GROUP BY baconnum;
-select count(*) from bacon;
-select count(*) from Actor;
 
---SELECT id FROM Actor WHERE firstname='Kevin' AND lastname='Bacon';
+--useful queries
+--DELETE FROM BACON;
+--select baconnum, count(*) From BACON GROUP BY baconnum;
+--select count(*) from bacon;
