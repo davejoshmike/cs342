0c9b91fdecf7b4af34640f48bca8d8aef5ab83f0
David Michel <djm43@calvin.edu>
2017-04-27 14:07:54 -0400

    Finished Lab Exercise 11.3

--- 11postrelational/Lab11/src/Hello.java
+++ 11postrelational/Lab11/src/Hello.java
@@ -1,16 +1,50 @@
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 
 /**
  * Created by djm43 on 4/21/2017.
  */
-@Path("/hello")
+@Path("/hello/api")
 public class Hello {
-    @GET
+    private String x;
 
-    @Produces("text/plain")
-    public String getClichedMessage() {
-        return "Hello!";
+    @GET
+    @Produces({"text/plain", "application/json"})
+    public String getAllMessage() {
+        return "Getting... ";
+    }
+
+    @GET
+    @Path("/{x}")
+    @Produces({"text/plain", "application/json"})
+    public String getMessage(@PathParam("x") String x) {
+        return "Getting: " + x;
+    }
+
+    @PUT
+    @Path("/{x}")
+    @Consumes({"text/plain", "application/json"})
+    @Produces({"text/plain", "application/json"})
+    public String setMessage(@PathParam("x") String x) {
+        this.x = x;
+        return "Putting: " + x;
+    }
+
+    @POST
+    @Consumes({"text/plain", "application/json"})
+    @Produces({"text/plain", "application/json"})
+    public String postMessage(String x) {
+        this.x = x;
+        return "Posting: " + x;
+    }
+
+    @DELETE
+    @Path("/{x}")
+    @Consumes({"text/plain", "application/json"})
+    @Produces({"text/plain", "application/json"})
+    public String deleteMessage(@PathParam("x") String x) {
+        String message = "Deleting: " + x;
+        this.x = null;
+        return message;
     }
 }
